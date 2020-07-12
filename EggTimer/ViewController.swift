956f31341c7b94963fa8868a6bc686bb9457bfa4
antmdev <antmilner@hotmail.com>
2020-07-12 12:00:24 +0100

    if statement to send number of minute

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    
 
-
+    @IBAction func hardnessAction(_ sender: UIButton ) {
+        
+//        print(sender.currentTitle)
+        
+        let hardness = sender.currentTitle
+        
+        if sender.currentTitle == "Hard" {
+            print(12)
+        }
+        else if sender.currentTitle == "Medium" {
+            print(7)
+        }
+        else if sender.currentTitle == "Soft" {
+            print(5)
+            
+        }
+        
+        
+    }
+    
+    
 }
